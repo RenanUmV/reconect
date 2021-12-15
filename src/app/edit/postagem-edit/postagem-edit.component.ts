080fcc5c2cf4771fc470713d3d7562272a3625d6
brunaguimaraesb <brunaguimaraes.santos@hotmail.com>
2021-12-15 20:37:10 -0300

    @Brunaguimaraesb [Task: Create Put Postagem and Update Feed]

---
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostagemModel } from 'src/app/model/PostagemModel';
+import { TemaModel } from 'src/app/model/TemaModel';
+import { PostagensService } from 'src/app/service/postagens.service';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-postagem-edit',
+  templateUrl: './postagem-edit.component.html',
+  styleUrls: ['./postagem-edit.component.css']
+})
+export class PostagemEditComponent implements OnInit {
+
+  postagem: PostagemModel = new PostagemModel()
+
+  tema: TemaModel = new TemaModel()
+  listaTemas: TemaModel[]
+  idTema: number
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostagensService,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      this.router.navigate(['/login'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdPostagem(id)
+    this.findAllTemas()
+  }
+
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp: PostagemModel) =>{
+      this.postagem = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: TemaModel)=>
+    {
+      this.tema = resp
+    })
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: TemaModel[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  atualizar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.postagemService.putPostagem(this.postagem).subscribe((resp: PostagemModel) => {
+      alert('Postagem atualizada com sucesso!')
+      this.router.navigate(['/feed'])
+    })
+  }
+
+}
